4d1cf67d069d38d9800fbf5ce23713ee0e8207f8
sandy <sandy@atinfo.com.tw>
2021-03-14 09:43:32 +0800

    新增 Router 和基本頁面

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Layout from "./pages/_Layout.page.jsx";
+import Home from "./pages/Home.page.jsx";
+import Notification from "./pages/Notification.page.jsx";
+import User from "./pages/User.page.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Layout>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/notification" component={Notification} />
+          <Route exact path="/user" component={User} />
+        </Layout>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
