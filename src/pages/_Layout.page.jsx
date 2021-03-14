ffef8b92588f10278f7fd63e9afb4b294a3b4dca
sandy <sandy@atinfo.com.tw>
2021-03-14 16:02:43 +0800

    新增 Menu

--- src/pages/_Layout.page.jsx
+++ src/pages/_Layout.page.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+const MenuBar = React.lazy(() => import('tools/MenuBar.tool.jsx'));
+
 export default function Layout(props) {
 
     return (
         <>
-            {props.children}
+            <CssBaseline />
+            <MenuBar />
+            <Container maxWidth="md">
+                <Box px={3}>
+                    {props.children}
+                </Box>
+            </Container>
         </>
     );
 }
---
+++ src/tools/MenuBar.tool.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import PersonIcon from '@material-ui/icons/Person';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    grow: {
+        flexGrow: 1,
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('xs')]: {
+            display: 'block',
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    desktopIconSize: {
+        fontSize: '30px',
+    },
+}));
+
+export default function MenuBar() {
+    const classes = useStyles();
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const mobileMenuId = 'primary-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton aria-label="Goods" color="inherit">
+                    <CardGiftcardIcon />
+                </IconButton>
+                <p>Goods</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton aria-label="Notifications" color="inherit">
+                    <Badge badgeContent={11} color="secondary">
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                <p>Notifications</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton aria-label="User" color="inherit">
+                    <PersonIcon />
+                </IconButton>
+                <p>User</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static">
+                <Container maxWidth="md">
+                    <Toolbar px={0}  mx={0}>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            Simple Life
+                    </Typography>
+                        <div className={classes.grow} />
+                        <div className={classes.sectionDesktop}>
+                            <IconButton aria-label="Goods" color="inherit" component={RouterLink} to="/">
+                                <CardGiftcardIcon className={classes.desktopIconSize} />
+                            </IconButton>
+                            <IconButton aria-label="Notifications" color="inherit" component={RouterLink} to="/notification">
+                                <Badge badgeContent={17} color="secondary">
+                                    <NotificationsIcon className={classes.desktopIconSize} />
+                                </Badge>
+                            </IconButton>
+                            <IconButton aria-label="User" color="inherit" component={RouterLink} to="/user">
+                                <PersonIcon className={classes.desktopIconSize} />
+                            </IconButton>
+                        </div>
+                        <div className={classes.sectionMobile}>
+                            <IconButton
+                                aria-label="show more"
+                                aria-controls={mobileMenuId}
+                                aria-haspopup="true"
+                                onClick={handleMobileMenuOpen}
+                                color="inherit"
+                            >
+                                <MoreIcon />
+                            </IconButton>
+                        </div>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            {renderMobileMenu}
+        </div>
+    );
+}
