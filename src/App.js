5ec7f640499e18dccf6ff8b7f86f1ecc3693e204
sandy <sandy@atinfo.com.tw>
2021-03-14 09:55:08 +0800

    新增 lazy import

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Layout from "./pages/_Layout.page.jsx";
-import Home from "./pages/Home.page.jsx";
-import Notification from "./pages/Notification.page.jsx";
-import User from "./pages/User.page.jsx";
+import Loading from './tools/Loading.tool.jsx';
+const Layout = React.lazy(() => import('./pages/_Layout.page.jsx'));
+const Home = React.lazy(() => import('./pages/Home.page.jsx'));
+const Notification = React.lazy(() => import('./pages/Notification.page.jsx'));
+const User = React.lazy(() => import('./pages/User.page.jsx'));
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Layout>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/notification" component={Notification} />
-          <Route exact path="/user" component={User} />
-        </Layout>
+        <Suspense fallback={<Loading />}>
+          <Layout>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/notification" component={Notification} />
+            <Route exact path="/user" component={User} />
+          </Layout>
+        </Suspense>
       </Switch>
     </BrowserRouter>
   );
