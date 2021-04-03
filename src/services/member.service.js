d74789aa203ae2371d947ef66889dfbc239a9e8b
sandy <sandy@atinfo.com.tw>
2021-04-03 16:00:45 +0800

    調整創建 member 時寫入 firebase data 的 Id

--- src/services/member.service.js
+++ src/services/member.service.js
@@ -2,8 +2,9 @@ import BaseDataService from "services/_base.service";
 let collection = "/members";
 
 class Member {
-    constructor(dt) {
+    constructor(id, dt) {
         this.data = {
+            id: id,
             uid: dt.uid,
             profile: {
                 name: dt !== undefined ? dt.profile.name : "",
@@ -11,7 +12,6 @@ class Member {
                 photoURL: dt !== undefined ? dt.profile.photoURL : "",
             },
             registerDate: dt.registerDate !== undefined ? dt.registerDate : Date.now(),
-            publishedCount: dt.publishedCount !== undefined ? dt.publishedCount : 0,
             messageBoard: dt.messageBoard !== undefined ? dt.messageBoard : "每天一點點，邁向簡單幸福生活。"
         }
     }
@@ -20,7 +20,11 @@ class Member {
 class MemberDataService {
 
     create(data) {
-        BaseDataService.create(collection, data);
+        return BaseDataService.create(collection, data);
+    }
+
+    update(id, data) {
+        return BaseDataService.update(collection, id, data);
     }
 
     getById(uid, user, setState) {
@@ -34,13 +38,26 @@ class MemberDataService {
         BaseDataService.query(collection, queryCondition).then(snapshot => {
             let member;
             if (snapshot.docs.length === 0) {
-                member = new Member(user).data;
-                this.create(member);
-                setState(member);
+                member = new Member("", user).data;
+
+                this.create(member)
+                    .then(function (docRef) {
+                        console.log("成功新增使用者，ID: ", docRef.id);
+                        member.id = docRef.id;
+                        let memberDataService = new MemberDataService();
+                        memberDataService.update(member.id, member);
+                        setState(member);
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    });
+
+                // this.create(member);
+                // setState(member);
             } else {
                 snapshot.forEach((item) => {
                     let dt = item.data();
-                    member = new Member(dt).data;
+                    member = new Member(dt.id, dt).data;
                     setState(member);
                 });
             }
@@ -63,8 +80,9 @@ class MemberDataService {
             // TODO:目前用迴圈塞資料，數量多時可能會有效能問題，要改寫法
             let chatMessageItems = messageItems;
             snapshot.forEach((item) => {
+                let id = item.id;
                 let dt = item.data();
-                let member = new Member(dt).data;
+                let member = new Member(id, dt).data;
                 chatMessageItems.forEach(i => {
                     if (i.memberIds.indexOf(dt.uid) > -1) {
                         i.chatMemberName = member.profile.name;
