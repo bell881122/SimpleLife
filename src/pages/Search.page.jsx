a9296f78a7dc52550fe4a02471674eb685180650
sandy <sandy@atinfo.com.tw>
2021-04-15 17:45:39 +0800

    補 Search.page.jsx

---
+++ src/pages/Search.page.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import moment from 'moment';
+
+import Box from '@material-ui/core/Box';
+
+import GoodDataService from "services/good.service";
+const SearchInput = React.lazy(() => import('components/Search/SearchInput.component.jsx'));
+const CardList = React.lazy(() => import('components/Card/CardList.component.jsx'));
+
+export default function Search() {
+    const { kw } = useParams();
+    const searchString = kw;
+    const [allPublishedGoods, setAllPublishedGoods] = React.useState();
+    const [searchGoods, setSearchGoods] = React.useState();
+
+    React.useEffect(() => {
+        let data = localStorage.getItem('SimpleLifeGoods');
+
+        if (data === null) {
+            setData();
+        } else {
+            let expireTime = JSON.parse(data)['expireTime'];
+            let now = moment(Date.now()).toDate();
+            let compare = moment(now, 'h:mma').isBefore(expireTime, 'h:mma');
+            if (compare) {
+                let cacheData = JSON.parse(data)['allGoods'];
+                let searchKwArr = searchString.split(" ");
+                let goodIds = Object.keys(cacheData).filter(key => searchKwArr.every(skr => cacheData[key].some(kw => kw.indexOf(skr) > -1)));
+                GoodDataService.getByIds(goodIds, setSearchGoods);
+            } else {
+                setData();
+            }
+        }
+
+        function setData() {
+            if (!allPublishedGoods) {
+                GoodDataService.query([{ key: "published", operation: "==", condition: true }], ["registerTimestamp", "desc"], setAllPublishedGoods);
+            } else {
+                let allGoods = {};
+                allPublishedGoods.forEach(good => {
+                    const arr1 = good.title.split(" ");
+                    const arr2 = good.tags;
+                    const keywords = arr1.concat(arr2);
+                    allGoods[good.id] = keywords;
+                });
+
+                let expireTime = moment(Date.now()).add(5, 'm').toDate();
+                console.log("expireTime", expireTime);
+                localStorage.setItem('SimpleLifeGoods', JSON.stringify({
+                    'expireTime': expireTime,
+                    'allGoods': allGoods
+                }));
+            }
+        }
+
+    }, [searchString, allPublishedGoods]);
+
+    return (
+        <Box pt={3} pb={8}>
+            <SearchInput searchString={searchString} />
+            <Box mt={3}>
+                <CardList
+                    title={`搜尋:${searchString}`}
+                    goods={searchGoods}
+                />
+            </Box>
+        </Box>
+    );
+}
