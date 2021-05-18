0e7a61d765a68d953732714039bcedbe0afa13d4
sandy <sandy@atinfo.com.tw>
2021-05-18 22:16:26 +0800

    調整物品卡片詳情不要吃掉圖片

--- src/components/Card/CardDetail.component.jsx
+++ src/components/Card/CardDetail.component.jsx
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column',
     },
     cardMedia: {
-        paddingTop: '56.25%', // 16:9
+        paddingTop: '100%',
     },
     cardContent: {
         flexGrow: 1,
